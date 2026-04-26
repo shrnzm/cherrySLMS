d733bd48c4932d945bf5c906ad334ca328457c84
shrnzm <shrnzm1709@gmail.com>
2026-04-26 13:03:17 +0800

    refactor: implement cache API and fixed delete logic

--- StudentManager.java
+++ StudentManager.java
@@ -1,9 +1,11 @@
+package com.mycompany.slms;
+
 import java.util.*;
 
 class StudentManager {
-
     //Array to store Student objects
     private Student[] studentArray = new Student[500];
+    
     private int studentCount = 0;
     private SuggestionAPI cacheAPI;
 
@@ -11,15 +13,15 @@ class StudentManager {
         this.cacheAPI = cacheAPI;
     }
 
-    //Add Student Method
+    // Add Student Method
     public void addStudent(Scanner sc) {
-        //Check if array is full
+        // Check if array is full
         if (studentCount >= studentArray.length) {
             System.out.println("\nStudent list is full. Cannot add new student.");
             return;
         }
 
-        //Create new Student object
+        // Create new Student object
         Student newStudent = new Student();
 
         System.out.print("\nEnter First Name: ");
@@ -31,14 +33,14 @@ class StudentManager {
         System.out.print("Enter Student ID: ");
         String studentId = sc.nextLine();
         
-        //Check for duplicates
+        // Check for duplicates
         if (searchStudent(studentId) != null) {
             System.out.println("Error! Student ID already exists.");
             return;
         }
         newStudent.setStudentId(studentId);
 
-        //Check email for correct format
+        // Check email for correct format
         while (true) {
             System.out.print("Enter Email: ");
             String emailInput = sc.nextLine();
@@ -50,7 +52,7 @@ class StudentManager {
             }
         }
 
-        //Check phone number for correct format
+        // Check phone number for correct format
         while (true) {
             System.out.print("Enter Phone Number: ");
             String phoneInput = sc.nextLine();
@@ -62,17 +64,17 @@ class StudentManager {
             }
         }
 
-        //Add new student to end of array
+        // Add new student to end of array
         studentArray[studentCount] = newStudent;
         studentCount++;
         
-        //Add new student ID to cache
+        // Add new student ID to cache
         cacheAPI.cacheStudent(newStudent.getStudentId());
 
         System.out.println("\nStudent added successfully.");
     }
 
-    //Search Student Method
+    // Search Student Method
     public Student searchStudent(String studentId) {
         for (int i = 0; i < studentCount; i++) {
             if (studentArray[i].getStudentId().equalsIgnoreCase(studentId)) {
@@ -82,7 +84,7 @@ class StudentManager {
         return null;
     }
 
-    //Edit Student Method
+    // Edit Student Method
     public void editStudent(String studentId, Scanner sc) {
         Student student = searchStudent(studentId);
 
@@ -93,26 +95,28 @@ class StudentManager {
 
         System.out.println("\nStudent found.\n");
 
-        //Display details of student to edit
+        // Display details of student to edit
         displayStudent(student);
 
         System.out.print("\nAre you sure you want to edit this student? (Y/N): ");
         String confirm = sc.nextLine();
 
-        //Check for confirmation
+        // Check for confirmation
         if (confirm.equalsIgnoreCase("N")) {
             System.out.println("Edit cancelled.");
             return;
         }
 
-        //Enter new details of student
+        System.out.println();
+        
+        // Enter new details of student
         System.out.print("Enter New First Name: ");
         student.setFirstName(sc.nextLine());
 
         System.out.print("Enter New Last Name: ");
         student.setLastName(sc.nextLine());
 
-        //Check email for correct format
+        // Check email for correct format
         while (true) {
             System.out.print("Enter New Email: ");
             String emailInput = sc.nextLine();
@@ -124,7 +128,7 @@ class StudentManager {
             }
         }
 
-        //Check phone number for correct format
+        // Check phone number for correct format
         while (true) {
             System.out.print("Enter New Phone Number: ");
             String phoneInput = sc.nextLine();
@@ -138,11 +142,11 @@ class StudentManager {
 
         System.out.println("\nStudent updated successfully.\nUpdated details:\n");
 
-        //Display edited student details
+        // Display edited student details
         displayStudent(student);
     }
 
-    //Delete Student Method
+    // Delete Student Method
     public void deleteStudent(String studentId, Scanner sc) {
         Student student = searchStudent(studentId);
 
@@ -153,43 +157,50 @@ class StudentManager {
 
         System.out.println("\nStudent found.\n");
 
-        //Display details of student to delete
+        // Display details of student to delete
         displayStudent(student);
 
         System.out.print("\nAre you sure you want to delete this student? (Y/N): ");
         String confirm = sc.nextLine();
 
-        //Check for confirmation
+        // Check for confirmation
         if (confirm.equalsIgnoreCase("N")) {
             System.out.println("Deletion cancelled.");
             return;
         }
 
-        //Find index of student to delete
+        // Find index of student to delete
         int index = -1;
         for (int i = 0; i < studentCount; i++) {
-            if (studentArray[i].getStudentId().equals(studentId)) {
+            if (studentArray[i] != null && studentArray[i].getStudentId().equals(studentId)) {
                 index = i;
                 break;
             }
         }
+        
+        // If student is not found
+        if (index == -1) {
+            System.out.println("\nStudent not found.");
+            return;
+        }
 
-        //Shift all students after deleted one to the left
+        // Shift all students after deleted one to the left
         for (int i = index; i < studentCount - 1; i++) {
             studentArray[i] = studentArray[i + 1];
         }
 
-        //Empty the last element in studentArray
+        // Empty the last element in studentArray
         studentArray[studentCount - 1] = null;
         studentCount--;
-
-        System.out.println("\nStudent deleted successfully!\nUpdated student list:");
-
-        //Display all students
+        
+        cacheAPI.removeStudent(studentId);
+        System.out.println("\nStudent deleted successfully!\nUpdated student list:\n");
+        
+        // Display all students
         viewAllStudents();
     }
 
-    //Display Student Method
+    // Display Student Method
     public void displayStudent(Student student) {
         if (student != null) {
             System.out.println("Student ID: " + student.getStudentId());
@@ -199,10 +210,10 @@ class StudentManager {
         }
     }
 
-    //View All Students Method
+    // View All Students Method
     public void viewAllStudents() {
         if (studentCount == 0) {
-            System.out.println("No students available.");
+            System.out.println("\nNo students available.");
             return;
         }
         for (int i = 0; i < studentCount; i++) {
